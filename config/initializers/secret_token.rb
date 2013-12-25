ba831ba44b1600cd6c5862be93375b8de1f9600a
Shay-Jahen Merritte <shay@merritte.com>
2013-12-24 20:35:32 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clothesandbuttons::Application.config.secret_key_base = 'd8fe858fd5f3eaae5e8a0496109de777e0458af7fa05f05e2119b90e90ab55500a803423a37ff427576b1f986ec81c2fb6fc4d3aac5fe9af5b646157f04b279c'
